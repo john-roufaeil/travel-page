21625b64725f6f1c122b340452e870c172b414b0
john-roufaeil <john.flouis1@gmail.com>
2024-07-09 22:24:05 +0300

    feat: resizing buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
+function toggleTheme(theme) {
+    if (theme === 'light') {
+        document.body.classList.remove('body-dark');
+    } else {
+        document.body.classList.add('body-dark');
+    }
+}
+
+function toggleSize(size) {
+    document.documentElement.style.setProperty('font-size', `${size}`);
+}
+
+document.getElementById('toggle-light-theme').addEventListener('click', function () {
+    toggleTheme('light');
+});
+
+document.getElementById('toggle-dark-theme').addEventListener('click', function () {
+    toggleTheme('dark');
+});
+
+document.getElementById('toggle-large-size').addEventListener('click', function () {
+    toggleSize('18px');
+});
+
+document.getElementById('toggle-base-size').addEventListener('click', function () {
+    toggleSize('16px');
+});
+
+document.getElementById('toggle-small-size').addEventListener('click', function () {
+    toggleSize('14px');
 });
